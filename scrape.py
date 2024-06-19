a0b1f24f7ccc5a1d5b82156de8dd0e228bf80ab3
dg1223 <alavi1223@hotmail.com>
2024-06-19 16:18:51 -0400

    feature: removed headers from get request

--- scrape.py
+++ scrape.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 
 def get_camera_ids(url, headers, api):
-    response = requests.get(api, headers=headers).json()
+    response = requests.get(api).json()
     cameras = [camera for camera in response["data"]]
     camera_ids = [camera["id"] for camera in cameras]
     
@@ -24,14 +24,13 @@ def get_images_from_cctv(url, camera_ids):
 
 if __name__ == "__main__":
     url = "https://511on.ca/map/cctv/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0",
-    }
+    
     '''
     TODO: images should be retrieved asynchronously in batches
     '''
     start = 0
     length = 5
+    
     api = f'https://511on.ca/List/GetData/Cameras?query={{"columns":[],"start":{start},"length":{length}}}'
     
     camera_ids = get_camera_ids(url, headers, api)
