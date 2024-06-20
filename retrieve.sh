665e60f3acfec3f1d1218a4e837a0d64b3c22d0b
dg1223 <alavi1223@hotmail.com>
2024-06-20 14:48:28 -0400

    feature: receive starting point as second argument for testing and parallelism

--- retrieve.sh
+++ retrieve.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# @param 1: total_cameras (number of images to retrieve)
+# @param 2: starting point to start fetching images from
+
 get_total_cameras() {
     python retrieve_images.py get_total_cameras
 }
 
+# Server is rate limited to 100 requests per API call.
+# Lower batch size slows down asynchronous processing.
+BATCH_SIZE=100
+START_POINT=0
+
 if [[ $# -eq 1 ]]; then
     TOTAL_CAMERAS=$1
+elif [[ $# -eq 2 ]]; then
+    TOTAL_CAMERAS=$1
+    START_POINT=$2
 else
     get_total_cameras
     TOTAL_CAMERAS=$(cat total_cameras.txt)
@@ -17,9 +28,7 @@ if ! [[ "${TOTAL_CAMERAS}" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
-# Server is rate limited to 100 requests per API call.
-# Lower batch size slows down asynchronous processing.
-BATCH_SIZE=100
+echo -e "\nTOTAL_CAMERAS = ${TOTAL_CAMERAS}, START_POINT = ${START_POINT}"
 
 for((i = 0; i < TOTAL_CAMERAS; i += BATCH_SIZE)); do
     IMAGES_LEFT_TO_RETRIEVE=$((TOTAL_CAMERAS-i))
@@ -28,7 +37,7 @@ for((i = 0; i < TOTAL_CAMERAS; i += BATCH_SIZE)); do
         BATCH_SIZE=${IMAGES_LEFT_TO_RETRIEVE}
     fi
 
-    START=$i
+    START=$((START_POINT+i))
     LENGTH=${BATCH_SIZE}
 
     # Calculate batch number
